fba68415e4201d746fe7b7ab0fe13d9aea97d74a
dimzcraft <151798683+dimz-craft@users.noreply.github.com>
2023-12-16 20:13:01 +0700

    Create main.js

---
+++ asset/script/main.js
@@ -0,0 +1,45 @@
+var sidebar = document.getElementById("sidebar");
+var overlay = document.getElementById("sidebar-close-overlay");
+
+function showSidebar() {
+  sidebar.style.display = "block";
+  overlay.style.display = "block";
+}
+
+function closeSidebar() {
+  sidebar.style.display = "none";
+  overlay.style.display = "none";
+}
+
+function visitWebButton() {
+   window.location.href = '/Download.html';
+}
+
+function dcServerButton() {
+   window.location.href = 'https://discord.com/invite/Xfh7cCWZeW';
+}
+
+function waGroupButton() {
+   window.location.href = 'https://chat.whatsapp.com/FwlQU1fQovj0O0Wwvth9Qn';
+}
+
+
+function sYoutubeButton() {
+   window.location.href = 'https://youtube.com/@Dimz_craft?si=ooLLDcQWe0rIvp-D';
+}
+
+function sTiktokButton() {
+   window.location.href = 'https://www.tiktok.com/@dmzcraft?_t=8iDzIvSe3Am&_r=1';
+}
+
+function sFacebookButton() {
+   window.location.href = 'https://www.facebook.com/profile.php?id=100044587080743&mibextid=ZbWKwL';
+}
+
+function sTwiterButton() {
+   window.location.href = 'https://twitter.com/DimzCraft?t=PKkdwNJPAByekN0rz7VRCA&s=09';
+}
+
+function sDonateButton() {
+   window.location.href = 'https://trakteer.id/DimzCraft/';
+}
